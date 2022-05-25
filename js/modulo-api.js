9fd9a882c28c8187634d90d2b30253c6fb98bcd9
Luca Chieregatti <25lucachiere@gmail.com>
2022-05-25 15:48:22 +0200

    usando group

--- js/modulo-api.js
+++ js/modulo-api.js
@@ -23,7 +23,7 @@ export const listNightmareS = () => {
     })
 }*/
 //-----------------------------------------------------------------------------------
-import {BASE_URL} from "./config.js"
+/*import {BASE_URL} from "./config.js"
 export const apiList = (s, type) => {
     const url = BASE_URL + `s=${s}&type=${type}`;
     fetch(url)
@@ -31,5 +31,25 @@ export const apiList = (s, type) => {
     .then((results) => {
         const items = results.Search;
         console.log(items);
+    });
+}*/
+//-------------------------------------------------------------------------------
+import {BASE_URL} from "./config.js"
+export const apiList = (s, type) => {
+    const url = BASE_URL + `s=${s}&type=${type}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((results) => {
+        const items = results.Search;
+        viewItems(items);
+    });
+}
+const viewItems = (items) => {
+    items.map((item) => {
+        console.group(item.Title);
+        console.log(item.Year);
+        console.log(item.Type);
+        console.log(item.Poster);
+        console.groupEnd();
     });
 }
