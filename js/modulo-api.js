cb0c05ff2369098b0c1264d3ca4692aa92d4cdf9
Luca Chieregatti <25lucachiere@gmail.com>
2022-05-25 11:56:38 +0200

    con solo le serie tv

--- js/modulo-api.js
+++ js/modulo-api.js
@@ -1,6 +1,17 @@
 //export const URL = "https://www.omdbapi.com/?apikey=23887cb6&s=terminator&type=series HTTP/1.1";
-
+/*
 const URL_LIST_NIGHTMARE = "https://www.omdbapi.com/?apikey=23887cb6&s=nightmare";
+
+export const listNightmare = () => {
+    fetch(URL_LIST_NIGHTMARE)
+    .then((response) => response.json())
+    .then((results) => {
+        const movies = results.Search;
+        console.log(movies);
+    })
+}
+*/
+const URL_LIST_NIGHTMARE = "https://www.omdbapi.com/?apikey=23887cb6&s=nightmare&type=series";
 
 export const listNightmare = () => {
     fetch(URL_LIST_NIGHTMARE)
